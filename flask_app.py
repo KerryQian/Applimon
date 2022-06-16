dd776c453d611d7984df97eec77a45303888a805
Kerry Qian <kerryqian@hotmail.com>
2022-06-16 16:37:37 +1000

    testing

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import requests
 from rhino3dm import *
 
 app = Flask(__name__)
@@ -7,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello from kerry"
+    return "hello from kerry fuck"
